570e1ecd4864d6a5f3ab498db5030a801f747004
Buutyful <mantelli_git@libero.it>
2024-10-19 01:25:05 +0200

    Added react client

--- tests/ProgrammingBuddies.Application.UnitTests/obj/Debug/net8.0/ProgrammingBuddies.Application.UnitTests.AssemblyInfo.cs
+++ tests/ProgrammingBuddies.Application.UnitTests/obj/Debug/net8.0/ProgrammingBuddies.Application.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammingBuddies.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4761d6fdfa90ea1f0e17508cffe51af2afa6351c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182c52887dac8381fe1e98d3561d332eac84ea22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammingBuddies.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammingBuddies.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
